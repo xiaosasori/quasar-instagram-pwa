552ce4d340fcca774ab2007a005ff2368de11cea
Nguyen Minh Anh <xiaosasori@gmail.com>
2021-05-03 01:46:31 +0900

    add camera

---
+++ src/pages/Camera.vue
@@ -0,0 +1,183 @@
+<template>
+  <q-page class="constraint-more q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <video
+        v-show="!imageCaptured"
+        ref="video"
+        class="full-width"
+        autoplay
+      />
+      <canvas
+        v-show="imageCaptured"
+        ref="canvas"
+        class="full-width"
+        height="240"
+      />
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn
+        v-if="hasCameraSupport"
+        color="grey-10"
+        icon="eva-camera"
+        round
+        size="lg"
+        @click="captureImage"
+      />
+      <q-file
+        v-else
+        v-model="imageUpload"
+        @input="captureImageFallback"
+        label="Choose an image"
+        accept="image/*"
+        outlined
+      >
+        <template v-slot:prepend>
+          <q-icon name="eva-attach-outline" />
+        </template>
+      </q-file>
+      <div class="justify-center row q-ma-md">
+        <q-input
+          v-model="post.caption"
+          class="col col-sm-6"
+          label="Caption"
+          dense
+        />
+      </div>
+      <div class="justify-center row q-ma-md">
+        <q-input
+          v-model="post.location"
+          class="col col-sm-6"
+          label="Location"
+          dense
+        >
+          <template #append>
+            <q-btn
+              round
+              dense
+              flat
+              icon="eva-navigation-2-outline"
+            />
+          </template>
+        </q-input>
+      </div>
+      <div class="justify-center row q-mt-lg">
+        <q-btn
+          color="primary"
+          label="Post Image"
+          rounded
+          unelevated
+        />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { uid } from 'quasar'
+export default {
+  name: 'PageCamera',
+  data() {
+    return {
+      post: {
+        id: uid(),
+        caption: '',
+        location: '',
+        photo: null,
+        date: Date.now(),
+      },
+      imageCaptured: false,
+      imageUpload: [],
+      hasCameraSupport: true
+    }
+  },
+  methods: {
+    initCamera() {
+      navigator.mediaDevices
+        .getUserMedia({
+          video: true,
+        })
+        .then((stream) => {
+          this.$refs.video.srcObject = stream
+        })
+        .catch(error => {
+          this.hasCameraSupport = false
+        })
+    },
+    captureImage() {
+      let video = this.$refs.video
+      let canvas = this.$refs.canvas
+      canvas.width = video.getBoundingClientRect().width
+      canvas.height = video.getBoundingClientRect().height
+      let context = canvas.getContext('2d')
+      context.drawImage(video, 0, 0, canvas.width, canvas.height)
+      this.imageCaptured = true
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL())
+      this.disableCamera()
+      // fetch(this.post.photo).then(async res => this.post.photo = await res.blob())
+    },
+    captureImageFallback(file) {
+      this.post.photo = file
+
+      let canvas = this.$refs.canvas
+      let context = canvas.getContext('2d')
+
+      var reader = new FileReader()
+      reader.onload = event => {
+        var img = new Image()
+        img.onload = () => {
+          canvas.width = img.width
+          canvas.height = img.height
+          context.drawImage(img,0,0)
+          this.imageCaptured = true
+        }
+        img.src = event.target.result
+      }
+      reader.readAsDataURL(file)
+    },
+    disableCamera() {
+      this.$refs.video.srcObject.getVideoTracks().forEach(track => {
+        track.stop()
+      })
+    },
+    dataURItoBlob(dataURI) {
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+      var byteString = atob(dataURI.split(',')[1]);
+
+      // separate out the mime component
+      var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+      // write the bytes of the string to an ArrayBuffer
+      var ab = new ArrayBuffer(byteString.length);
+
+      // create a view into the buffer
+      var ia = new Uint8Array(ab);
+
+      // set the bytes of the buffer to the correct values
+      for (var i = 0; i < byteString.length; i++) {
+          ia[i] = byteString.charCodeAt(i);
+      }
+
+      // write the ArrayBuffer to a blob, and you're done
+      var blob = new Blob([ab], {type: mimeString});
+      return blob;
+
+    }
+  },
+  mounted() {
+    this.initCamera()
+  },
+  beforeDestroy() {
+    if (this.hasCameraSupport) {
+      this.disableCamera()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.camera-frame {
+  border: 2px solid $grey-10;
+  border-radius: 10px;
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,107 @@
+<template>
+  <q-page class="constraint q-pa-md">
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="card-post q-mb-md"
+          flat
+          bordered
+        >
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label class="text-bold">xiaosasori</q-item-label>
+              <q-item-label caption>
+                {{post.location}}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator />
+          <q-img :src="post.imageUrl" />
+          <q-card-section>
+            <div>{{post.caption}}</div>
+            <div class="text-caption text-grey">{{niceDate(post.date)}}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-4 large-screen-only">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">xiaosasori</q-item-label>
+            <q-item-label caption>
+              location
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import {date} from 'quasar'
+export default {
+  name: 'PageHome',
+  data () {
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: 'Golden Gate Bridge',
+          date: Date.now(),
+          location: 'Hanoi, Vietnam',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+          id: 2,
+          caption: 'Golden Gate Bridge',
+          date: Date.now(),
+          location: 'Hanoi, Vietnam',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+          id: 3,
+          caption: 'Golden Gate Bridge',
+          date: Date.now(),
+          location: 'Hanoi, Vietnam',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+          id: 4,
+          caption: 'Golden Gate Bridge',
+          date: Date.now(),
+          location: 'Hanoi, Vietnam',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        }
+      ]
+    }
+  },
+  methods: {
+    niceDate(value) {
+      return date.formatDate(value, 'MMMM D h:mmA')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.card-post {
+  .q-img {
+    min-height: 200px;
+  }
+}
+</style>
